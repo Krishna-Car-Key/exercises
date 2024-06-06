4b445765ca19f0e34e4d096c53ba69c4ef2fe3ee
krishna <krishnakarki1996@gmail.com>
2024-06-06 15:40:01 +0545

    implement the methods of purchase class

--- purchase_books/main.py
+++ purchase_books/main.py
@@ -1,4 +1,5 @@
 import pandas
+from pdf_generator import generate_receipt
 
 
 df = pandas.read_csv("articles.csv", dtype={'id': str})
@@ -8,6 +9,7 @@ class Book:
     def __init__(self, book_id):
         self.book_id = book_id
         self.stock = df.loc[df['id'] == book_id, 'in stock'].squeeze()
+        self.book_name = df.loc[df['id'] == book_id, 'name'].squeeze()
 
     def order(self):
         df.loc[df['id'] == book_id, 'in stock'] = self.stock - 1
@@ -21,8 +23,14 @@ class Book:
 
 
 class PurchaseReceipt:
-    def generate(self):
-        pass
+    def __init__(self, customer_name, book_object):
+        self.customer_name = customer_name.title()
+        self.book_object = book_object
+        self.price = df.loc[df['id'] == self.book_object.book_id, 'price'].squeeze()
+
+    def generate_pdf(self):
+        generate_receipt(self.customer_name, self.book_object.book_name, self.price, self.book_object.book_id)
+        return print("Thanks for shopping with us!!")
 
 
 print(df)
@@ -31,7 +39,7 @@ book = Book(book_id)
 if book.stock_available():
     book.order()
     name = input("Enter your name : ")
-    purchase_receipt = PurchaseReceipt()
-    print(purchase_receipt.generate())
+    purchase_receipt = PurchaseReceipt(customer_name=name, book_object=book)
+    purchase_receipt.generate_pdf()
 else:
     print("The book is out of stock")
