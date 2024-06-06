8f9cfa74c8557ca3ee7984f1f7b2c6558349dd7f
krishna <krishnakarki1996@gmail.com>
2024-06-06 14:23:09 +0545

    implement logic for program

--- purchase_books/main.py
+++ purchase_books/main.py
@@ -1,8 +1,28 @@
+import pandas
+
+
+df = pandas.read_csv("articles.csv")
+
+
 class Book:
     def order(self):
         pass
 
-class Purchase:
-    def receipt(self):
+    def stock_of(self):
         pass
 
+
+class PurchaseReceipt:
+    def generate(self):
+        pass
+
+
+book_id = input("Enter the id of the book to purchase: ")
+book = Book(book_id)
+if book.stock_of() > 0:
+    book.order()
+    name = input("Enter your name : ")
+    purchase_receipt = PurchaseReceipt()
+    print(purchase_receipt.generate())
+else:
+    print("The book is out of stock")
