189755818fdc7a15f75d0bbcd91dc78ca3598a1a
krishna <krishnakarki1996@gmail.com>
2024-06-06 14:52:04 +0545

    implement the methods of book class

--- purchase_books/main.py
+++ purchase_books/main.py
@@ -1,15 +1,23 @@
 import pandas
 
 
-df = pandas.read_csv("articles.csv")
+df = pandas.read_csv("articles.csv", dtype={'id': str})
 
 
 class Book:
-    def order(self):
-        pass
+    def __init__(self, book_id):
+        self.book_id = book_id
+        self.stock = df.loc[df['id'] == book_id, 'in stock'].squeeze()
 
-    def stock_of(self):
-        pass
+    def order(self):
+        df.loc[df['id'] == book_id, 'in stock'] = self.stock - 1
+        df.to_csv("articles.csv", index=False)
+
+    def stock_available(self):
+        if self.stock > 0:
+            return True
+        else:
+            return False
 
 
 class PurchaseReceipt:
@@ -17,9 +25,10 @@ class PurchaseReceipt:
         pass
 
 
+print(df)
 book_id = input("Enter the id of the book to purchase: ")
 book = Book(book_id)
-if book.stock_of() > 0:
+if book.stock_available():
     book.order()
     name = input("Enter your name : ")
     purchase_receipt = PurchaseReceipt()
