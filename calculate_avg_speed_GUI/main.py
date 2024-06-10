d366505df676e6496c62e9907158ce096c498f2c
krishna <krishnakarki1996@gmail.com>
2024-06-10 23:10:41 +0545

    create button and add widgets to grid

--- calculate_avg_speed_GUI/main.py
+++ calculate_avg_speed_GUI/main.py
@@ -17,3 +17,21 @@ class SpeedCalculator(QWidget):
         self.metric_button = QComboBox()
         self.metric_button.addItem("Km")
         self.metric_button.addItem("Miles")
+
+        button = QPushButton("Calculate")
+        button.clicked.connect(self.calculate)
+
+        # Adding widgets
+        grid.addWidget(distance_label, 0, 0)
+        grid.addWidget(self.distance_line_edit, 0, 1)
+        grid.addWidget(time_label, 1, 0)
+        grid.addWidget(self.time_line_edit, 1, 1)
+        grid.addWidget(metric_label, 0, 2)
+        grid.addWidget(self.metric_button, 1, 2)
+        grid.addWidget(button, 2, 0, 1, 2)
+        grid.addWidget(self.output, 3, 0, 1, 3)
+
+        self.setLayout(grid)
+
+    def calculate(self):
+        pass
